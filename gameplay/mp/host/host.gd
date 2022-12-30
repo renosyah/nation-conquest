9730865e0a5a281c5370c43e56c4a38ae38d2ff5
renosyah <syahputrareno975@gmail.com>
2022-12-30 17:50:42 +0700

    axe & spear better 3d mesh

--- gameplay/mp/host/host.gd
+++ gameplay/mp/host/host.gd
@@ -1,7 +1,7 @@
 extends BaseGameplay
 
 const squads = [
-	preload("res://data/squad_data/squads/archer_squad.tres"),
+	#preload("res://data/squad_data/squads/archer_squad.tres"),
 	preload("res://data/squad_data/squads/axeman_squad.tres"),
 	preload("res://data/squad_data/squads/militia_squad.tres"),
 	preload("res://data/squad_data/squads/spearman_squad.tres"),
