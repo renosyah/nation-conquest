32d46f67ac0deb3c22df2ef16cd277f6d0a6b597
renosyah <syahputrareno975@gmail.com>
2023-01-03 13:46:27 +0700

    add more condition

--- data/counter/CounterData.gd
+++ data/counter/CounterData.gd
@@ -23,40 +23,46 @@ func get_attack_modifier(tier_to_counter :int, role_to_counter :int, current_dam
 		# got 25 % bonus damage
 		bonus += int(current_damage * 0.25)
 		
-		# check round arround counter
-		if unit_role == 0 and role_to_counter == 3:
-			unit_role = 4
-			
-		elif role_to_counter == 0 and unit_role == 3:
-			role_to_counter = 4
-		
-		if unit_role + 1 == role_to_counter:
-			# lose 50 % bonus damage
-			bonus -= int(current_damage * 0.50)
-			
-		elif unit_role - 1 == role_to_counter:
-			# got 50 % bonus damage
-			bonus += int(current_damage * 0.50)
+		bonus += _get_attack_modifier_by_role(
+			tier_to_counter, role_to_counter, current_damage
+		)
 		
 	# current tier is lower
-	else:
+	elif unit_tier < tier_to_counter:
+		
 		# lose 25 % bonus damage
 		bonus = bonus - int(current_damage * 0.25)
 		
-		# check round arround counter
-		if unit_role == 0 and role_to_counter == 3:
-			unit_role = 4
-			
-		elif role_to_counter == 0 and unit_role == 3:
-			role_to_counter = 4
+		bonus += _get_attack_modifier_by_role(
+			tier_to_counter, role_to_counter, current_damage
+		)
 		
-		if unit_role + 1 == role_to_counter:
-			# lose 50 % bonus damage
-			bonus -= int(current_damage * 0.50)
-			
-		elif unit_role - 1 == role_to_counter:
-			# got 50 % bonus damage
-			bonus += int(current_damage * 0.50)
-			
+	else:
+		bonus += _get_attack_modifier_by_role(
+			tier_to_counter, role_to_counter, current_damage
+		)
+		
 	var final_damage :int = current_damage + bonus
 	return final_damage if final_damage > 0 else 1
+
+func _get_attack_modifier_by_role(tier_to_counter :int, role_to_counter :int, current_damage :int) -> int:
+	var _bonus = 0
+	var _unit_role = unit_role
+	var _role_to_counter = role_to_counter
+	
+	# check round arround counter
+	if unit_role == 0 and role_to_counter == 3:
+		_unit_role = 4
+		
+	elif role_to_counter == 0 and unit_role == 3:
+		_role_to_counter = 4
+	
+	if _unit_role + 1 == _role_to_counter:
+		# lose 50 % bonus damage
+		_bonus -= int(current_damage * 0.50)
+		
+	elif _unit_role - 1 == _role_to_counter:
+		# got 50 % bonus damage
+		_bonus += int(current_damage * 0.50)
+		
+	return _bonus
