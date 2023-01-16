ac4b72882e1c3bb8a384b5028b991725e703bea4
renosyah <syahputrareno975@gmail.com>
2023-01-16 09:31:01 +0700

    change player name in setting in main menu

--- assets/ui/setting_menu/setting_menu.gd
+++ assets/ui/setting_menu/setting_menu.gd
@@ -1,9 +1,13 @@
 extends Control
 
-onready var _sfx_sound_setting_icon = $SafeArea/VBoxContainer2/HBoxContainer2/sfx_setting/TextureRect
+onready var _sfx_sound_setting_icon = $SafeArea/VBoxContainer2/HBoxContainer3/sfx_setting/TextureRect
+onready var _input_name = $input_name
+onready var _player_name_ui = $SafeArea/VBoxContainer2/HBoxContainer2/VBoxContainer/Label4
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	_input_name.visible = false
+	_player_name_ui.text = Global.player.player_name
 	check_sfx_setting()
 	
 func check_sfx_setting():
@@ -20,4 +24,9 @@ func _on_sfx_setting_pressed():
 func _on_exit_pressed():
 	visible = false
 
+func _on_name_setting_pressed():
+	_input_name.visible = true
 
+func _on_input_name_on_continue(_player_name, html_color):
+	Global.player.player_name = _player_name
+	_player_name_ui.text = Global.player.player_name
